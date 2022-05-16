77a9ad16177e010d21d97e0bccab815538a73e2b
Mateusz Cybulski <235847@edu.p.lodz.pl>
2022-05-16 17:36:58 +0200

    20. Menu with edit

--- JavaFX_tutorial/src/main/java/com/example/javafx_tutorial/PracticeMenu.java
+++ JavaFX_tutorial/src/main/java/com/example/javafx_tutorial/PracticeMenu.java
@@ -5,6 +5,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.SeparatorMenuItem;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
@@ -23,6 +24,8 @@ public class PracticeMenu extends Application {
      * @throws Exception - exception.
      * @method fileMenu.getItems().add(new MenuItem()) - this method adds MenuItem into dropdown Menu.
      * @method menuBar.getMenus().addAll() - setting defined Menu's into MenuBar.
+     * @method paste.setDisable(true) - unable MenuItem.
+     * @method editMenu.getItems().add(new SeparatorMenuItem()) - adds separator.
      */
     @Override
     public void start(Stage stage) throws Exception {
@@ -31,9 +34,19 @@ public class PracticeMenu extends Application {
 
         Menu fileMenu =  new Menu("File");
         fileMenu.getItems().add(new MenuItem("New"));
+        fileMenu.getItems().add(new SeparatorMenuItem());
         fileMenu.getItems().add(new MenuItem("Open"));
+
+        Menu editMenu =  new Menu("Edit");
+        editMenu.getItems().add(new MenuItem("Copy"));
+        editMenu.getItems().add(new SeparatorMenuItem());
+        MenuItem paste = new MenuItem("Paste");
+        paste.setOnAction(actionEvent -> System.out.println("Pasted"));
+        paste.setDisable(true);
+        editMenu.getItems().add(paste);
+
         MenuBar menuBar = new MenuBar();
-        menuBar.getMenus().addAll(fileMenu);
+        menuBar.getMenus().addAll(fileMenu,editMenu);
 
         layout = new BorderPane();
         layout.setTop(menuBar);
