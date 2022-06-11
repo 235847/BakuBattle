67734cf04f0640c348ec9ff47883361401a41f96
Wilhelm Osada <73279204+WilhelmOsada@users.noreply.github.com>
2022-06-11 11:07:31 +0200

    Update HpProgress.java

--- demo/src/main/java/com/example/demo/HpProgress.java
+++ demo/src/main/java/com/example/demo/HpProgress.java
@@ -2,45 +2,80 @@ package com.example.demo;
 
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.geometry.Side;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.transform.Rotate;
 
 import java.net.URL;
 
 public class HpProgress {
     private AnchorPane MainStage;
     private ImageView hpbar;
-    public HpProgress(AnchorPane pane,int positionX,int positionY)
+    private int team;
+    public HpProgress(AnchorPane pane, int positionX, int positionY, int side)
     {
+        // ----- README -----
+        //side moze miec wartosc 0 albo 1 (drużyna A i drużyna B)
+        //positionX to offset od krawedzi bocznej ekranu
+        //positionY to offset od krawędzi górnej ekranu
+        //przy side -1 offset liczony jest od lewego boku paska hp
+        //przy side 1 offset liczony jest od prawego boku paska hp
+        //offset od gory liczony jest od gornej krawedzi paska hp
+        //gdyby paski nie byly na rowno wysokosciowo trzeba pobawic sie
+        //wartoscia dodawana przy rotacji (ctrl + f: rotacja)
+        team = side;
         MainStage = pane;
         ImageView imageView1 = new ImageView();
         URL urll1 = this.getClass().getResource("HP_BACK.png");
         Image im1 = new Image(String.valueOf(urll1));
         imageView1.setImage(im1);
-        imageView1.setX(positionX);
-        imageView1.setY(positionY);
+        int offsetX;
+        int offsetY;
+        if(side == 1)
+        {
+            offsetX = (1920 - positionX);
+        }
+        else
+        {
+            offsetX = (positionX);
+        }
+        offsetY = positionY;
+
+        imageView1.setX(offsetX);
+        imageView1.setY(offsetY);
+
         MainStage.getChildren().add(imageView1);
 
         hpbar = new ImageView();
         URL urll = this.getClass().getResource("HP_FRONT.png");
         Image im = new Image(String.valueOf(urll));
         hpbar.setImage(im);
-        hpbar.setX(positionX + 4);
-        hpbar.setY(positionY + 4);
+        hpbar.setX(offsetX + 4);
+        hpbar.setY(offsetY + 4);
 
         MainStage.getChildren().add(hpbar);
 
         hpbar.setFitWidth(249);
-        hpbar.setPreserveRatio(false);
-        hpbar.setPickOnBounds(true);
+        if(side == 1)
+        {
+            hpbar.getTransforms().add(new Rotate(180,offsetX,offsetY + 30)); //rotacja
+            imageView1.getTransforms().add(new Rotate(180,offsetX,offsetY + 30)); //rotacja wartosci (default 30) muszą byc rowne sobie, zwiekszenie obnizy pasek, zmiejszenie podwyzszy
+        }
+        hpbar.getLayoutX();
+        hpbar.getLayoutY();
     }
 
     private double calculate_width(Number max, Number current)
     {
-        if(current.intValue() == 0)
+        if(current.doubleValue() == 0)
         {
             return 0.1;
+        }
+        else if(current.doubleValue() > max.doubleValue())
+        {
+            return 249;
         }
         else
         {
