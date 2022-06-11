01bf5998bebe00ef3ef1663e74ec26f5eef24f4a
Wilhelm Osada <73279204+WilhelmOsada@users.noreply.github.com>
2022-06-11 11:07:53 +0200

    Update HelloController.java

--- demo/src/main/java/com/example/demo/HelloController.java
+++ demo/src/main/java/com/example/demo/HelloController.java
@@ -4,17 +4,9 @@ import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
-import javafx.scene.control.ProgressBar;
 import javafx.scene.control.Slider;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Background;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -25,13 +17,16 @@ public class HelloController implements Initializable{
     @FXML
     private Slider slider;
 
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        HpProgress bar = new HpProgress(MainStage,200,200);
+        HpProgress bar = new HpProgress(MainStage,200,200,-1);
+        HpProgress bar2 = new HpProgress(MainStage,200,200,1);
 
         slider.valueProperty().addListener(new ChangeListener<Number>() {
             public void changed(ObservableValue<?extends Number> observable, Number oldValue, Number newValue){
                 bar.setValue(100, newValue);
+                bar2.setValue(100,newValue);
             }
         });
     }
